873df7feddfc91b1fb71103d96ba3532f8efbced
ngan <ngan@s3corp.com.vn>
2023-02-13 10:30:39 +0700

    A1_destructuring

---
+++ src/page/detracturing.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+const Destructuring = () => {
+  //exe
+  let a, b, rest;
+  // Swapping
+  [a, b] = [10, 20];
+
+  console.log(a);
+  // Expected output: 10
+
+  console.log(b);
+  // Expected output: 20
+
+  [a, b, ...rest] = [10, 20, 30, 40, 50];
+
+  console.log(rest);
+  // Expected output: Array [30, 40, 50]
+
+  const [e, g, ...c] = [1, 2, 3, 4, 5];
+  console.log(e, g, c); //1, 2, [3, 4, 5]
+  // Variable assignment
+
+  //when we use rest api when return array or object use destructuring effective then
+  //exe
+  const res = [1, 2, 3, 4]; //res.response();
+  //const [a, b, c] = res
+  //console.log(a, b, c);//1 2 3
+  //Nested object and array destructuring
+  //In this case, I recommend you to use it because this is the case a lot in our code.
+  const blogs = {
+    anonystick: [
+      {
+        pageFacebook: "Tip javascript Viet Nam",
+        likes: 4789,
+        daily: 1323,
+      },
+    ],
+  };
+
+  const {
+    anonystick: [{ pageFacebook: namePage, likes: numLikes, daily: numDaily }],
+  } = blogs;
+
+  console.log(namePage, numLikes, numDaily); //Tip javascript Viet Nam, 4789, 1323
+  return (
+    <>
+      <p>function_arrowfunction </p>
+    </>
+  );
+};
+
+export default Destructuring;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Destructuring from "../page/detracturing";
 import Funtion_arrowFuntion from "../page/funtion_arrowFuntion";
+import Spread_restOperator from "../page/spread_restOperator";
 import Var_let_cont from "../page/var_let_const";
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Var_let_cont />} />
       <Route path="/funtion" element={<Funtion_arrowFuntion />} />
+      <Route path="/spread" element={<Spread_restOperator />} />
+      <Route path="/destructuring" element={<Destructuring/>} />
+    
     </Routes>
   );
 };
