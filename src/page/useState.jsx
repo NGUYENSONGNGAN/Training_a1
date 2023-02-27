c77bc6090bb3ccb8bcaa16d2a4996b2793b04fa4
ngan <ngan@s3corp.com.vn>
2023-02-27 13:39:23 +0700

    update A1_useState

--- src/page/useState.jsx
+++ src/page/useState.jsx
@@ -2,6 +2,22 @@ import React, { useState } from "react";
 const UseState = () => {
   /* useState */
   const [counter, setCouter] = useState(1);
+  const [job, setjob] = useState("");
+  const [jobs, setjobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+    return storageJobs;
+  });
+  const hanleSubmit = () => {
+    setjobs((prev) => {
+      const newJob = [...prev, job];
+
+      //Save to local store
+      const jsonJobs = JSON.stringify(newJob);
+      localStorage.setItem("jobs", jsonJobs);
+      return newJob;
+    });
+    setjob("");
+  };
   return (
     <div style={{ padding: "50px" }}>
       <p>useState </p>
@@ -12,6 +28,19 @@ const UseState = () => {
       >
         increase
       </button>
+      <div style={{ padding: 32 }}>
+        <input
+          type="text"
+          value={job}
+          onChange={(e) => setjob(e.target.value)}
+        />
+        <button onClick={hanleSubmit}>Add</button>
+        <ul>
+          {jobs.map((job, index) => (
+            <li key={index}>{job}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
